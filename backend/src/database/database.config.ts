225986e05069da836201068511edf7f22bea8eaf
DeemoHarlos <cces4415@gmail.com>
2024-11-10 18:06:59 +0800

    add environment configuration and update database settings to use environment variables

--- backend/src/database/database.config.ts
+++ backend/src/database/database.config.ts
@@ -5,11 +5,11 @@ import { User } from '@/user/user.entity'
 
 export const databaseConfig: DataSourceOptions = {
   type: 'postgres',
-  host: '127.0.0.1',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'cardio_test',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'cardio',
   entities: [User, Post],
   synchronize: false,
   migrations: ['./src/database/migrations/*.ts'],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import 'reflect-metadata'
 
+import '@/env.config'
+
 import { AppModule } from '@/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(process.env.BACKEND_PORT ?? 6789)
 }
 
 bootstrap()
